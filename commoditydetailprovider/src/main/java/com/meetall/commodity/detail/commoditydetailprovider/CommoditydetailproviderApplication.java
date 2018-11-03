18a32df65a6a645eb38a37b3bc9beff0093a6051
sunKefan <m17603452718@163.com>
2018-11-03 16:03:41 +0800

    商品详情生产者

---
+++ commoditydetailprovider/src/main/java/com/meetall/commodity/detail/commoditydetailprovider/CommoditydetailproviderApplication.java
@@ -0,0 +1,12 @@
+package com.meetall.commodity.detail.commoditydetailprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommoditydetailproviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommoditydetailproviderApplication.class, args);
+    }
+}
