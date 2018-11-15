f19acf52d7431ce67507e904186a8f8d91a24c19
sunKefan <m17603452718@163.com>
2018-11-15 11:38:26 +0800

    consumer

---
+++ meetallsyatem/shopingcartconsumer/src/main/java/com/example/shopingcartconsumer/ShopingcartconsumerApplication.java
@@ -0,0 +1,12 @@
+package com.example.shopingcartconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopingcartconsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopingcartconsumerApplication.class, args);
+    }
+}
