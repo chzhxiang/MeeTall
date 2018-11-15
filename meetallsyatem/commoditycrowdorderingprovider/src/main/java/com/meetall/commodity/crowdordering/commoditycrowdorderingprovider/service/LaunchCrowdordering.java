18684891a319ca68f061c2364f7ce1e02f730623
sunKefan <m17603452718@163.com>
2018-11-15 11:12:35 +0800

    1

--- meetallsyatem/commoditycrowdorderingprovider/src/main/java/com/meetall/commodity/crowdordering/commoditycrowdorderingprovider/service/LaunchCrowdordering.java
+++
@@ -1,4 +0,0 @@
-package com.meetall.commodity.crowdordering.commoditycrowdorderingprovider.service;
-
-public interface LaunchCrowdordering {
-}
--- meetallsyatem/commoditydetailprovider/src/main/java/com/meetall/commodity/detail/commoditydetailprovider/service/CommoditySkuService.java
+++
@@ -1,4 +0,0 @@
-package com.meetall.commodity.detail.commoditydetailprovider.service;
-
-public interface CommoditySkuService {
-}
